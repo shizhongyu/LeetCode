e07339cfaf30f496cb2ac9f90f1c6eb935df565d
microli <microli@tencent.com>
2022-09-05 16:52:58 +0800

    验证回文字符串。

---
+++ src/IsPalindrome.java
@@ -0,0 +1,34 @@
+public class IsPalindrome {
+
+    public static void main(String[] args) {
+        boolean palin = isPalindrome("aba");
+        System.out.printf("" + palin);
+    }
+
+
+    public static boolean isPalindrome(String s) {
+        if (s.isEmpty()) {
+            return true;
+        }
+        int head = 0, tail = s.length() - 1;
+        char cHead, cTail;
+        while (head <= tail) {
+            cHead = s.charAt(head);
+            cTail = s.charAt(tail);
+            if (!Character.isLetterOrDigit(cHead)) {
+                head++;
+            } else if (!Character.isLetterOrDigit(cTail)) {
+                tail--;
+            } else {
+                if (Character.toLowerCase(cHead) != Character.toLowerCase(cTail)) {
+                    return false;
+                }
+                head++;
+                tail--;
+            }
+        }
+
+        return true;
+    }
+
+}
