7cfc889476c8e20357270732a2d42b58366861c1
microli <microli@tencent.com>
2022-09-10 15:56:10 +0800

    LeetCode

--- src/LeetCode912.java
+++ src/LeetCode912.java
@@ -2,12 +2,20 @@ import java.util.Arrays;
 
 public class LeetCode912 {
     public static void main(String[] args) {
-        int[] nums = {5, 2, 3, 1};
+        int[] nums = {-1, 2, -8, -10};
         sort(nums, 0, nums.length - 1);
         System.out.println(Arrays.toString(nums));
     }
 
 
+    /**
+     * 快速排序
+     * https://www.bilibili.com/video/BV1K44y1k79z?spm_id_from=333.337.search-card.all.click&vd_source=4f6e3c370a3e292341287c0fd0e1b24c
+     *
+     * @param nums
+     * @param start
+     * @param end
+     */
     public static void sort(int[] nums, int start, int end) {
         if (start >= end) {
             return;
@@ -17,9 +25,9 @@ public class LeetCode912 {
         int right = end;
 
         //注意点 全部是小于等于
+        int mid = nums[(start + end) / 2];
         while (left <= right) {
             // 基准点选取中间
-            int mid = nums[(start + end) / 2];
             while (left <= right && nums[left] < mid) {
                 left++;
             }
