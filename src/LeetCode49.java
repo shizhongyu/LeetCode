c20287ad72dd53654fab79caf43f6ced518fc957
microli <1966957866@qq.com>
2022-10-06 18:05:02 +0800

    code

---
+++ src/LeetCode49.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class LeetCode49 {
+    public static void main(String[] args) {
+        String[] strs  = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            int [] ac = new int[26];
+            for (char c : str.toCharArray()) {
+                ac[c - 'a']++;
+            }
+//            Arrays.sort(ac);
+            String temp = String.valueOf(ac);
+            List<String> list = map.getOrDefault(temp, new ArrayList<>());
+            list.add(str);
+            map.put(temp, list);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
