b28aee06c7d99b4374705fc47f708105f49cedd0
microli <1966957866@qq.com>
2022-10-07 14:25:28 +0800

    加一

---
+++ src/LeetCode66.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class LeetCode66 {
+    public static void main(String[] args) {
+        int [] nums = {1,2,3};
+        System.out.println(Arrays.toString(plusOne(nums)));
+    }
+
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if(digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+}
